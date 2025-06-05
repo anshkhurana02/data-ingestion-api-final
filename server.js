1685f8ef6c1cfe7abc816cb32d3c709ebbcb449d
Ansh <khuraana.ansh@gmail.com>
2025-06-05 11:43:40 +0530

    fixed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('./jobs/jobProcessor'); // start background processor
 const app = require('./app');
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
